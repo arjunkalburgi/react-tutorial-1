585db5ba42b33cb552aaaa9ff4a8290b6ff0b47f
Andrew Fontaine <andrew@afontaine.ca>
2017-02-02 22:54:48 -0500

    sorry

--- challenge3.js
+++ challenge3.js
@@ -1,35 +1,100 @@
-class FizzBuzzDisplay extends React.Component {
+"use strict";
+const redux_1 = require("redux");
+const react_redux_1 = require("react-redux");
+const react_1 = require("react");
+const react_dom_1 = require("react-dom");
+// REDUX
+const ADD = "ADD";
+const MINUS = "MINUS";
+/*
+    reducer - takes in state and action
+    keeping an "action" allows you to keep history.
+    
+    Reducer delievers to
+    
+*/
+function numberReducer(state, action) {
+    // return new state
+    // action = { type: "ADD" }
+    if (action.type == ADD) {
+        return {
+            number: state.number + 1
+        };
+    }
+    if (action.type == MINUS) {
+        return {
+            number: state.number - 1
+        };
+    }
+    return state;
+}
+let store = redux_1.createStore(numberReducer);
+class FizzBuzzDisplay extends react_1.Component {
     render() {
         if ((this.props.number) % 3 == 0) {
-            return React.createElement("div", null, "\"Fizz\"");
+            return react_1.default.createElement("div", null, "\"Fizz\"");
         }
         else if ((this.props.number) % 5 == 0) {
-            return React.createElement("div", null, "\"Buzz\"");
+            return react_1.default.createElement("div", null, "\"Buzz\"");
         }
         else {
-            return React.createElement("div", null, this.props.number);
+            return react_1.default.createElement("div", null, this.props.number);
         }
     }
 }
-class FizzBuzz extends React.Component {
+class FizzBuzz extends react_1.Component {
     constructor(props) {
         super(props);
-        this.state = { number: 1 };
-        this.minus = this.minus.bind(this);
-        this.plus = this.plus.bind(this);
-    }
-    minus() {
-        this.setState(prevState => ({ number: prevState.number - 1 }));
-    }
-    plus() {
-        this.setState(prevState => ({ number: prevState.number + 1 }));
     }
     render() {
-        return (React.createElement("div", null, 
-            React.createElement(FizzBuzzDisplay, {number: this.state.number}), 
-            React.createElement("button", {onClick: this.minus}, "-"), 
-            React.createElement("button", {onClick: this.plus}, "+")));
+        return (react_1.default.createElement("div", null,
+            react_1.default.createElement(ConnectedFizzBuzzDisplay, null),
+            react_1.default.createElement("button", { onClick: this.props.minus }, "-"),
+            react_1.default.createElement("button", { onClick: this.props.plus }, "+")));
     }
 }
-ReactDOM.render(React.createElement(FizzBuzz, null), document.getElementById("root"));
+class BuzzFizzDisplay extends react_1.Component {
+    render() {
+        if ((this.props.number) % 3 == 0) {
+            return react_1.default.createElement("div", null, "\"Buzz\"");
+        }
+        else if ((this.props.number) % 5 == 0) {
+            return react_1.default.createElement("div", null, "\"Fizz\"");
+        }
+        else {
+            return react_1.default.createElement("div", null, this.props.number);
+        }
+    }
+}
+class BuzzFizz extends react_1.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (react_1.default.createElement("div", null,
+            react_1.default.createElement(ConnectedBuzzFizzDisplay, null),
+            react_1.default.createElement("button", { onClick: this.props.minus }, "-"),
+            react_1.default.createElement("button", { onClick: this.props.plus }, "+")));
+    }
+}
+function mapDispatchToFizzBuzzProps(dispatch, ownProp) {
+    return {
+        plus: () => dispatch({ type: ADD }),
+        minus: () => dispatch({ type: MINUS })
+    };
+}
+function mapStateToFizzBuzzDisplayProps(state, ownProp) {
+    return {
+        number: state.number
+    };
+}
+let fizzBuzzFactory = react_redux_1.connect(() => { }, mapDispatchToFizzBuzzProps);
+let ConnectedFizzBuzz = fizzBuzzFactory(FizzBuzz);
+let fizzBuzzDisplayFactory = react_redux_1.connect(mapStateToFizzBuzzDisplayProps);
+let ConnectedFizzBuzzDisplay = fizzBuzzDisplayFactory(FizzBuzzDisplay);
+let ConnectedBuzzFizz = fizzBuzzFactory(BuzzFizz);
+let ConnectedBuzzFizzDisplay = fizzBuzzDisplayFactory(FizzBuzzDisplay);
+react_dom_1.render(react_1.default.createElement(react_redux_1.Provider, { store: store },
+    react_1.default.createElement(ConnectedFizzBuzz, null),
+    react_1.default.createElement(ConnectedBuzzFizz, null)), document.getElementById("root"));
 //# sourceMappingURL=challenge3.js.map
--- challenge3.tsx
+++ challenge3.tsx
@@ -1,12 +1,56 @@
+import { createStore, Action } from 'redux';
+import { connect, Provider } from 'react-redux';
+import * as React from 'react';
+import { Component } from 'react';
+import { render } from 'react-dom';
+// REDUX
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 interface FizzBuzzState {
+    number: number
+}
+
+/*
+    reducer - takes in state and action 
+    keeping an "action" allows you to keep history. 
+    
+    Reducer delievers to 
+    
+*/ 
+function numberReducer(state: FizzBuzzState = { number: 0}, action: Action): FizzBuzzState {
+    // return new state
+    // action = { type: "ADD" }
+    if (action.type == ADD) {
+        return {
+            number: state.number + 1
+        }; 
+    } 
+    if (action.type == MINUS) {
+        return {
+            number: state.number - 1
+        }; 
+    }
+    return state;
+}
+
+interface ReduxStore<T> {
+    state: T;
+    subscribe: (f: () => void) => void;
+    dispatch: (action: Action) => void;
+}
+
+// let store = createStore(numberReducer);
+const store = createStore(numberReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// REACT
+
+interface FizzBuzzDisplayProps {
 	number: number
 }
 
-interface FizzBuzzProps {
-	number: number
-}
-
-class FizzBuzzDisplay extends React.Component<FizzBuzzProps,{}> {
+class FizzBuzzDisplay extends Component<FizzBuzzDisplayProps,{}> {
 	render() {
 		if ((this.props.number) % 3 == 0) {
 			return <div>"Fizz"</div>
@@ -19,25 +63,70 @@ class FizzBuzzDisplay extends React.Component<FizzBuzzProps,{}> {
 	}
 }
 
-class FizzBuzz extends React.Component<{},FizzBuzzState> {
+interface FizzBuzzProps {
+    plus: () => void;
+    minus: () => void;
+}
+
+class FizzBuzz extends Component<FizzBuzzProps, {}> {
 	constructor(props) {
-		super(props); 
-		this.state = {number: 1}
-		this.minus = this.minus.bind(this);
-		this.plus = this.plus.bind(this);
-	}
-	minus() {
-		this.setState(prevState => ({number: prevState.number-1}))
-	}
-	plus() {
-		this.setState(prevState => ({number: prevState.number+1}));
+		super(props);
 	}
 	render() {
 		return (<div>
-					<FizzBuzzDisplay number={this.state.number} />
-					<button onClick={this.minus}>-</button><button onClick={this.plus}>+</button>
+					<ConnectedFizzBuzzDisplay />
+					<button onClick={this.props.minus}>-</button><button onClick={this.props.plus}>+</button>
 				</div>)
 	}
 }
 
-ReactDOM.render(<FizzBuzz />, document.getElementById("root"));
+class BuzzFizzDisplay extends Component<FizzBuzzDisplayProps,{}> {
+	render() {
+		if ((this.props.number) % 3 == 0) {
+			return <div>"Buzz"</div>
+		}
+		else if ((this.props.number) % 5 == 0) {
+			return <div>"Fizz"</div>
+		} else {
+			return <div>{this.props.number}</div>
+		}
+	}
+}
+
+class BuzzFizz extends Component<FizzBuzzProps, {}> {
+	constructor(props) {
+		super(props);
+	}
+	render()  {
+		return (<div>
+					<ConnectedBuzzFizzDisplay />
+					<button onClick={this.props.minus}>-</button><button onClick={this.props.plus}>+</button>
+				</div>)
+	}
+}
+
+function mapDispatchToFizzBuzzProps(dispatch, ownProp) {
+    return {
+        plus: () => dispatch({ type: ADD }),
+        minus: () => dispatch({ type: MINUS })
+    }
+}
+
+function mapStateToFizzBuzzDisplayProps(state, ownProp) {
+    return {
+        number: state.number
+    }
+}
+
+
+let fizzBuzzFactory = connect(() => {}, mapDispatchToFizzBuzzProps);
+let ConnectedFizzBuzz = fizzBuzzFactory(FizzBuzz);
+
+let fizzBuzzDisplayFactory = connect(mapStateToFizzBuzzDisplayProps);
+let ConnectedFizzBuzzDisplay = fizzBuzzDisplayFactory(FizzBuzzDisplay);
+
+let ConnectedBuzzFizz = fizzBuzzFactory(BuzzFizz); 
+let ConnectedBuzzFizzDisplay = fizzBuzzDisplayFactory(FizzBuzzDisplay);
+
+render(<Provider store={store}><ConnectedFizzBuzz /></Provider>, document.getElementById("root"));
+
